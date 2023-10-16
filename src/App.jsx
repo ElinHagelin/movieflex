064859a873681367d10b6c3d2c6cd56bd665f9ac
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-10-16 17:15:59 +0200

    added logo and heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <header>
+      <img className='logo' src="../assets/images/cinema+digital+film+movie+multimedia+video+icon-1320186751465241961.svg" alt="logo" />
+     <h1>MovieFlex</h1>
+    </header>
     </>
   )
 }
+
+// TODO: Byt loggan till något snyggare
+
+
 
 export default App
