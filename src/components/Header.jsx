a45dadf7bf8c893a613efb1a315aa11044dce0bf
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-10-26 18:10:33 +0200

    corrected path to logo in Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
 		<header>
 			<motion.img
 				className='logo'
-				src='../public/images/logo.png'
+				src='../../public/images/logo.png'
 				alt='logo'
 				variants={logoVariants}
 				animate={animate}
